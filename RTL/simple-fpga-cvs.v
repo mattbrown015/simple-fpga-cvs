f766b00031acc7f16e1a6c3375d0c27504797885
Matt Brown <matt_d_br@hotmail.com>
2020-06-10 15:29:39 +0100

    Expanded design to control 5 LEDs with the 5 buttons.
    I followed the Flow Navigator in Vivado and this all seems to work.

--- RTL/simple-fpga-cvs.v
+++ RTL/simple-fpga-cvs.v
@@ -1,9 +1,21 @@
 `timescale 1ps / 1ps
 
 module simple_fpga_cvs(
-    input wire clock,
-    output wire clock_out
+    input wire in1,
+    output wire out1,
+    input wire in2,
+    output wire out2,
+    input wire in3,
+    output wire out3,
+    input wire in4,
+    output wire out4,
+    input wire in5,
+    output wire out5
     );
 
-    assign clock_out = clock;
+    assign out1 = in1;
+    assign out2 = in2;
+    assign out3 = in3;
+    assign out4 = in4;
+    assign out5 = in5;
 endmodule
--- Testbenches/simple-fpga-tb.v
+++ Testbenches/simple-fpga-tb.v
@@ -9,7 +9,11 @@
 module simple_fpga_cvs_tb();
     // 'reg' can store a logic state
     reg clock;
-    wire clock_out;
+    wire out1;
+    wire out2;
+    wire out3;
+    wire out4;
+    wire out5;
 
     always begin
         // '#' is unsynthesizable element that specifies simulation delay
@@ -20,5 +24,5 @@ module simple_fpga_cvs_tb();
         clock = 0;
     end
 
-    simple_fpga_cvs simple_fpga_cvs(clock, clock_out);
+    simple_fpga_cvs simple_fpga_cvs(clock, out1, clock, out2, clock, out3, clock, out4, clock, out5);
 endmodule
