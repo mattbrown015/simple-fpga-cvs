964a55e27f19c319d3a03e29e06ce2c92261431d
Matt Brown <matt_d_br@hotmail.com>
2020-06-10 12:18:18 +0100

    Created Verilog testbench.
    My first Verilog. Adding this code to the project means I can run a simulation and see the /clock/ in a waveform.
    
    I've added some pretty basic comments, this stuff is new to me so I'm going overboard!

---
+++ Testbenches/simple-fpga-tb.v
@@ -0,0 +1,21 @@
+// timescale directive to define meaning of timestep
+// `timescale <time_unit>/<time_precision>
+// The simulation log contained this message 'Time resolution is 1 ps'
+// regardless of the timescale set here. I'm guessing that there is
+// a global setting and making this the same will help the simulation.
+// Also guessing that it is easier if time unit and precision are the same.
+`timescale 1ps / 1ps
+
+module simple_fpga_cvs_tb();
+    // 'reg' can store a logic state
+    reg clock;
+
+    always begin
+        // '#' is unsynthesizable element that specifies simulation delay
+        #1 clock = !clock;
+    end
+
+    initial begin
+        clock = 0;
+    end
+endmodule
