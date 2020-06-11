1d83a3af85dc572323b2029fe0bb834e0ab76c3f
Matt Brown <matt_d_br@hotmail.com>
2020-06-11 15:23:19 +0100

    Added 300 MHz oscillator to the testbench.
    The KCU105 includes a 300 MHz oscillator. While I'm not sure what I want to do with it I've got a feeling it will be useful.
    
    Before I can use it in the design it needs to be added to the testbench.
    
    Oscillators are special, the have 2 pins, P and N.

--- Testbenches/simple-fpga-cvs-tb.sv
+++ Testbenches/simple-fpga-cvs-tb.sv
@@ -11,13 +11,26 @@ module simple_fpga_cvs_tb();
     reg clock;
     wire out[4:0];
 
+    reg osc_300;
+    wire osc_300_pn[1:0];
+
+    assign osc_300_pn = '{osc_300, !osc_300};
+
     always begin
         // '#' is unsynthesizable element that specifies simulation delay
         #1 clock = !clock;
     end
 
+    always begin
+        // The timescale precision is 1ps so the .666 will be ignored.
+        // Changing the timescale precision to 1fs makes it possible to see the .666ps in the waveform but I feel that 1fs precision is unnecessary
+        // I assume increasing the precision slows the simulation.
+        #1666.666 osc_300 = !osc_300; // 300 MHz, period is 3333.333 ps but the state change frequency is 600 MHz so delay is 1666.666
+    end
+
     initial begin
         clock = 0;
+        osc_300 = 0;
     end
 
     simple_fpga_cvs simple_fpga_cvs('{clock, clock, clock, clock, clock}, out);
