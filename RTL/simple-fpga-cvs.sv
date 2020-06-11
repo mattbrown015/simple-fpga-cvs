6d62b62888150da9b12fb97843d50c921745b80e
Matt Brown <matt_d_br@hotmail.com>
2020-06-11 14:20:23 +0100

    Changed design to use array.
    I'm not sure an array is the correct option here, it depends whether or not you think all the ins and outs are that closely related, but I'm experimenting with SystemVerilog.
    
    I believe that what I've done here wasn't possible in Verilog, as opposed to SystemVerilog.

--- RTL/simple-fpga-cvs.sv
+++ RTL/simple-fpga-cvs.sv
@@ -1,21 +1,9 @@
 `timescale 1ps / 1ps
 
 module simple_fpga_cvs(
-    input wire in1,
-    output wire out1,
-    input wire in2,
-    output wire out2,
-    input wire in3,
-    output wire out3,
-    input wire in4,
-    output wire out4,
-    input wire in5,
-    output wire out5
+    input wire in[4:0],
+    output wire out[4:0]
     );
 
-    assign out1 = in1;
-    assign out2 = in2;
-    assign out3 = in3;
-    assign out4 = in4;
-    assign out5 = in5;
+    assign out = in;
 endmodule
--- Testbenches/simple-fpga-cvs-tb.sv
+++ Testbenches/simple-fpga-cvs-tb.sv
@@ -9,11 +9,7 @@
 module simple_fpga_cvs_tb();
     // 'reg' can store a logic state
     reg clock;
-    wire out1;
-    wire out2;
-    wire out3;
-    wire out4;
-    wire out5;
+    wire out[4:0];
 
     always begin
         // '#' is unsynthesizable element that specifies simulation delay
@@ -24,5 +20,5 @@ module simple_fpga_cvs_tb();
         clock = 0;
     end
 
-    simple_fpga_cvs simple_fpga_cvs(clock, out1, clock, out2, clock, out3, clock, out4, clock, out5);
+    simple_fpga_cvs simple_fpga_cvs('{clock, clock, clock, clock, clock}, out);
 endmodule
