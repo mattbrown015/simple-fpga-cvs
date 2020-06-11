0a13e250b564b35d2328ce5557e0ae8d9da189e7
Matt Brown <matt_d_br@hotmail.com>
2020-06-11 15:27:26 +0100

    No need to 'begin' and 'end' for single line blocks.
    I don't know if this is a SystemVerilog thing, I originally copied the blocks from a Verilog tutorial but noticed others doing it this way.
    
    Also removed verbose comments, having committed them once I should remember!

--- Testbenches/simple-fpga-cvs-tb.sv
+++ Testbenches/simple-fpga-cvs-tb.sv
@@ -16,17 +16,8 @@ module simple_fpga_cvs_tb();
 
     assign osc_300_pn = '{osc_300, !osc_300};
 
-    always begin
-        // '#' is unsynthesizable element that specifies simulation delay
-        #1 clock = !clock;
-    end
-
-    always begin
-        // The timescale precision is 1ps so the .666 will be ignored.
-        // Changing the timescale precision to 1fs makes it possible to see the .666ps in the waveform but I feel that 1fs precision is unnecessary
-        // I assume increasing the precision slows the simulation.
-        #1666.666 osc_300 = !osc_300; // 300 MHz, period is 3333.333 ps but the state change frequency is 600 MHz so delay is 1666.666
-    end
+    always #1 clock = !clock;
+    always #1666.666 osc_300 = !osc_300; // 300 MHz, period is 3333.333 ps but the state change frequency is 600 MHz so delay is 1666.666
 
     initial begin
         clock = 0;
