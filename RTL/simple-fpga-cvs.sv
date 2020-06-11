aac007ad43942c05b3a19ef64b53295ba26c23b7
Matt Brown <matt_d_br@hotmail.com>
2020-06-11 16:13:19 +0100

    Wired up the KCU105's 300 MHz to the FPGA, connected it to an SMA connector.
    It appears that Vivado does some magic that I don't really understand for the simple GPIO, in the schematic the are IBUF things!
    
    AFAICT, the same magic doesn't work for the oscillator inputs and a buffer has to be added in the design.
    
    On the menu Tools|Language Templates I found the template code for the IBUFDS, "Verilog|Device Primitive Instantiation|Kintex Ultrascale|I/O|INPUT BUFFER|Differential Input Buffer".
    
    I can see the oscillator on the user SMA. I was expecting to see a digital signal, i.e. square wave, but it turns out it is the oscillator waveform. Not sure what this means yet!

--- RTL/simple-fpga-cvs.sv
+++ RTL/simple-fpga-cvs.sv
@@ -7,6 +7,15 @@ module simple_fpga_cvs(
     output wire osc_300_out
     );
 
+    // IBUFDS: Differential Input Buffer
+    //         Kintex UltraScale+
+    // Xilinx HDL Language Template, version 2018.3
+
+    IBUFDS IBUFDS_clk_300 (
+        .O(osc_300_out),   // 1-bit output: Buffer output
+        .I(osc_300_pn[0]),  // 1-bit input: Diff_p buffer input (connect directly to top-level port)
+        .IB(osc_300_pn[1])  // 1-bit input: Diff_n buffer input (connect directly to top-level port)
+    );
+
     assign out = in;
-    assign osc_300_out = osc_300_pn[0];
 endmodule
