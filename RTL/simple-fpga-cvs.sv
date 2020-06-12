511c2b8c6c09647097caa0a0c368531b29f0170a
Matt Brown <matt_d_br@hotmail.com>
2020-06-12 08:28:58 +0100

    Removed the boiler plate comments from the language templates.
    As the design grows they will start getting in the way and I know where they came from so can always revisit.

--- RTL/simple-fpga-cvs.sv
+++ RTL/simple-fpga-cvs.sv
@@ -10,35 +10,23 @@ module simple_fpga_cvs(
     wire osc_300;
     wire clk_fb;
 
-    // IBUFDS: Differential Input Buffer
-    //         Kintex UltraScale+
-    // Xilinx HDL Language Template, version 2018.3
-
     IBUFDS IBUFDS_inst (
-        .O(osc_300),        // 1-bit output: Buffer output
-        .I(osc_300_pn[0]),  // 1-bit input: Diff_p buffer input (connect directly to top-level port)
-        .IB(osc_300_pn[1])  // 1-bit input: Diff_n buffer input (connect directly to top-level port)
+        .O(osc_300),
+        .I(osc_300_pn[0]),
+        .IB(osc_300_pn[1])
     );
-
-    // MMCME3_BASE: Base Mixed Mode Clock Manager (MMCM)
-    //              Kintex UltraScale
-    // Xilinx HDL Language Template, version 2018.3
 
     MMCME3_BASE #(
-        .CLKFBOUT_MULT_F(2.0),      // Multiply value for all CLKOUT (2.000-64.000)
-        .CLKIN1_PERIOD(3.333),      // Input clock period in ns units, ps resolution (i.e. 33.333 is 30 MHz).
-        .CLKOUT0_DIVIDE_F(2.0)      // Divide amount for CLKOUT0 (1.000-128.000)
+        .CLKFBOUT_MULT_F(2.0),
+        .CLKIN1_PERIOD(3.333),
+        .CLKOUT0_DIVIDE_F(2.0)
     )
     MMCME3_BASE_inst (
-        // Clock Outputs outputs: User configurable clock outputs
-        .CLKOUT0(clk_out),     // 1-bit output: CLKOUT0
-        .CLKFBOUT(clk_fb),     // 1-bit output: Feedback clock
-        .CLKIN1(osc_300),      // 1-bit input: Clock
-        // Feedback inputs: Clock feedback ports
-        .CLKFBIN(clk_fb)       // 1-bit input: Feedback clock
+        .CLKOUT0(clk_out),
+        .CLKFBOUT(clk_fb),
+        .CLKIN1(osc_300),
+        .CLKFBIN(clk_fb)
     );
-
-    // End of MMCME3_BASE_inst instantiation
 
     assign out = in;
 endmodule
